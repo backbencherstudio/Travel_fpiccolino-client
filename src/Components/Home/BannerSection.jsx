d190cec815af3d9e6b4e7ec31c93be35ebec73fc
sr sohan <sr.sohan088@gmail.com>
2024-12-02 09:52:14 +0600

    add footer

--- src/Components/Home/BannerSection.jsx
+++ src/Components/Home/BannerSection.jsx
@@ -5,8 +5,8 @@ const BannerSection = () => {
       <div className="relative">
         <img className="  max-h-[80vh] cover w-full" src={banner} alt="" />
         <div className="absolute inset-0 bg-gradient-to-t to-[#000000c4] from-transparent "></div>
-        <div className="w-full text-center absolute z-10 top-[50%]">
-        <h3 className="text-[36px] lg:text-[56px] text-white font-sans font-bold top-[50%] z-20 max-w-[760px] mx-auto">Feel at Home Wherever You Roam</h3>
+        <div className="w-full text-center absolute top-[50%]">
+        <h3 className="text-[36px] lg:text-[56px] text-white font-sans font-bold top-[50%] max-w-[760px] mx-auto">Feel at Home Wherever You Roam</h3>
         </div>
       </div>
     </>
--- src/Shared/Footer.jsx
+++ src/Shared/Footer.jsx
@@ -1,19 +1,55 @@
 import React from "react";
 import logo from "../assets/logo.svg";
-import ParentComponent from "../Components/ParentComponent/ParentComponent";
+import ParentComponent from "../Shared/ParentComponent/ParentComponent";
+import stripe from "../assets/payment/stripe.png";
+import visa from "../assets/payment/visa.png";
+import paypal from "../assets/payment/paypal.png";
+import epay from "../assets/payment/epay.png";
 
 const Footer = () => {
   return (
-    <div className="bg-black">
+    <div className="bg-black text-white text-[]">
       <ParentComponent>
-        <div className="grid grid-cols-3">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 pt-20 pb-10 lg:gap-0 gap-10">
           <div>
             <img src={logo} alt="" />
           </div>
-          <div className=""></div>
-          <div></div>
+          <div className="flex justify-between">
+            <div className="text-[18px] font-medium">
+              <h1 className="text-[20px] font-medium mb-6">Quick Link</h1>
+              <p>About</p>
+              <p>Tour</p>
+              <p>Blog</p>
+              <p>FAQ</p>
+              <p>Contact</p>
+            </div>
+            <div className="text-[16px] font-normal max-w-[218px]">
+              <h1 className="text-[20px] font-medium mb-6">Contact Us</h1>
+              <p>We're Here to Help with Any Questions or Concerns</p>
+              <p>012-3456-7899</p>
+              <p>latuafuga@gmail.com</p>
+            </div>
+          </div>
+          <div className="flex lg:justify-end">
+            <h1 className="text-[20px] font-medium w-[330px] ">Newsletter </h1>
+          </div>
         </div>
       </ParentComponent>
+      <div className="border-t">
+        <ParentComponent>
+          <div className="flex justify-between py-6">
+            <p className="text-[16px] ">
+              Copyright © 2024 LA TUA FUGA LOWCOST . All rights reserved.
+            </p>
+            <div className="flex gap-2">
+              <img src={stripe} className="w-9 h-7" alt="" />
+              <img src={visa} alt="" className="w-9 h-7" />
+              <img className="bg-white h-7 w-9 rounded-md" src={paypal} alt="" />
+              <img className="bg-white h-7 w-9 rounded-md" src={epay} alt="" />
+            </div>
+          </div>
+        </ParentComponent>
+      </div>
     </div>
   );
 };
