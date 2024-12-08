07b9cbb901b6f9fcbaf967d0494d36591716c576
sr sohan <sr.sohan088@gmail.com>
2024-12-08 11:16:28 +0600

    add modal

--- src/Pages/Auth/OtpScreen.jsx
+++ src/Pages/Auth/OtpScreen.jsx
@@ -4,14 +4,15 @@ import heroImage from "../../assets/Images/HeroSection/heroImage.jpg";
 import logo from "../../assets/logo.svg";
 import { FaAngleLeft } from "react-icons/fa";
 import { useState } from "react";
+import lock from '../../assets/icons/lock.svg'
 const OtpScreen = () => {
-    const [otp, setOtp] = useState(["", "", "", "", "", ""]);
-  const navigate = useNavigate()
+  const [otp, setOtp] = useState(["", "", "", "", "", ""]);
+  const navigate = useNavigate();
   const handleOtpChange = (index, value, event) => {
     if (value.length > 1) return; // Allow only one character
-  
+
     const newOtp = [...otp];
-  
+
     if (event.key === "Backspace") {
       if (!value && index > 0) {
         document.getElementById(`otp-input-${index - 1}`).focus();
@@ -19,8 +20,8 @@ const OtpScreen = () => {
     } else {
       newOtp[index] = value;
       setOtp(newOtp);
-  
-       if (value && index < 5) {
+
+      if (value && index < 5) {
         document.getElementById(`otp-input-${index + 1}`).focus();
       }
     }
@@ -34,32 +35,86 @@ const OtpScreen = () => {
         <div className="h-full w-full md:col-span-3">
           <div className="my-40 lg:mx-20 m-5">
             <img src={logo} alt="" />
-            <p onClick={()=>navigate('/forgot-password')} className="text-[16px] cursor-pointer mt-10 flex items-center"><FaAngleLeft style={{fontSize:'20px'}}/>  Back</p>
+            <p
+              onClick={() => navigate("/forgot-password")}
+              className="text-[16px] cursor-pointer mt-10 flex items-center"
+            >
+              <FaAngleLeft style={{ fontSize: "20px" }} /> Back
+            </p>
             <h1 className="font-extrabold text-[32px] mt-8">Enter OTP</h1>
             <h5 className="text-[#72777F] text-[16px] mt-3">
-            We have share a code of your registered email address
+              We have share a code of your registered email address
             </h5>
             <div className="flex justify-between mt-8">
               {otp.map((digit, index) => (
-               <input
-               key={index}
-               id={`otp-input-${index}`}
-               size="small"
-               value={digit}
-               onChange={(e) => handleOtpChange(index, e.target.value, e)}
-               onKeyDown={(e) => handleOtpChange(index, e.target.value, e)}
-               className={`w-[60px] h-[60px] text-sm text-center border ${digit && 'border-[#35aff4]'} rounded-lg font-bold text-[24px]`}
-             />
-             
+                <input
+                  key={index}
+                  id={`otp-input-${index}`}
+                  size="small"
+                  value={digit}
+                  onChange={(e) => handleOtpChange(index, e.target.value, e)}
+                  onKeyDown={(e) => handleOtpChange(index, e.target.value, e)}
+                  className={`w-[60px] h-[60px] text-sm text-center border ${
+                    digit && "border-[#35aff4]"
+                  } rounded-lg font-bold text-[24px]`}
+                />
               ))}
             </div>
-          
-          
-            <button className="primary_bg p-3 w-full rounded-lg text-white text-[16px] font-semibold mt-6 ">Submit OTP</button>
-       
+
+            <button
+              onClick={() => document.getElementById("password_modal").showModal()}
+              className="primary_bg p-3 w-full rounded-lg text-white text-[16px] font-semibold mt-6 "
+            >
+              Submit OTP
+            </button>
           </div>
         </div>
       </div>
+      <dialog id="password_modal" className="modal">
+        <div className="modal-box text-center">
+        <h1 className="font-extrabold text-[32px] mt-8">Change Password</h1>
+          <p className="py-4">Enter your new password and submit it</p>
+          <div>
+              <p className="text-[18px] font-medium mt-5 text-start">Password</p>
+              <input
+                type="password"
+                placeholder="Enter New Password"
+                className="p-3 text-[16px] rounded-md w-full border border-[#D0D5DD]"
+              />
+            </div>
+          <button
+               onClick={() => document.getElementById("success_modal").showModal()}
+              className="primary_bg p-3 w-full rounded-lg text-white text-[16px] font-semibold mt-6 "
+            >
+              Change Password
+            </button>
+        </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
+      <dialog id="success_modal" className="modal">
+        <div className="modal-box text-center">
+          <div className="bg-[#fef7f7] inline-block  rounded-full">
+            <div className="bg-[#fce9e9] m-2.5 mb-1.5 inline-block rounded-full">
+            <div className="primary_bg p-5 m-2.5 rounded-full">
+           <img src={lock} alt="" />
+           </div>
+            </div>
+          </div>
+        <h1 className="font-extrabold text-[32px] mt-8"> Successfully Password Changed</h1>
+          <p className="py-4">Your password has been updated successfully</p>
+          <button
+              onClick={() => navigate('/login')}
+              className="primary_bg p-3 w-full rounded-lg text-white text-[16px] font-semibold mt-6 "
+            >
+              Back to Login
+            </button>
+        </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
     </ParentAuthComponent>
   );
 };
