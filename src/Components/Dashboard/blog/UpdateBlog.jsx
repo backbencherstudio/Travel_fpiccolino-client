6e7d46f439b4b3db824fb3f5f76277605cfc48ca
FozleRabbi1 <fozlerabbishuvo@gmail.com>
2024-12-12 11:55:30 +0600

    working on  blog update page

--- src/Components/Dashboard/blog/UpdateBlog.jsx
+++ src/Components/Dashboard/blog/UpdateBlog.jsx
@@ -1,16 +1,19 @@
+import CustomHeadingDashboard from "../../../Shared/CustomHeadingDashboard";
 
 const UpdateBlog = () => {
     return (
         <div>
-            <h1 className="text-[32px] font-semibold">Welcome, Wade</h1>
-            <p className="text-[#72777F]">
-                Manage your travel agency data easily with us
-            </p>
+           <CustomHeadingDashboard/>
+
+            <div className="mt-10 flex justify-between items-center "  >
+                <h2 className="text-[#141D2A] font-semibold text-[24px] " >Edit Blog</h2>
+                <button className="bg-[#E86731] text-[#FFFFFF] px-4 py-2 rounded-md " >Update Blog</button>
+            </div>
 
             <div>
-                <h2 className="text-[#141D2A] font-semibold text-[24px] " >Edit Blog</h2>
-                <button>Update Blog</button>
+                
             </div>
+
         </div>
     );
 };
