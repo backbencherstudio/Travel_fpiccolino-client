04bf8844780e4b5924c8c191814f42c8a1be750c
FozleRabbi1 <fozlerabbishuvo@gmail.com>
2024-12-12 11:44:53 +0600

    update

--- src/Components/Dashboard/blog/UpdateBlog.jsx
+++ src/Components/Dashboard/blog/UpdateBlog.jsx
@@ -2,7 +2,15 @@
 const UpdateBlog = () => {
     return (
         <div>
-            
+            <h1 className="text-[32px] font-semibold">Welcome, Wade</h1>
+            <p className="text-[#72777F]">
+                Manage your travel agency data easily with us
+            </p>
+
+            <div>
+                <h2 className="text-[#141D2A] font-semibold text-[24px] " >Edit Blog</h2>
+                <button>Update Blog</button>
+            </div>
         </div>
     );
 };
