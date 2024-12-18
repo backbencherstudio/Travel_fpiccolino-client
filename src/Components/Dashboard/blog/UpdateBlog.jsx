fb7bda5f7a99d29330f2b3bafe296792a3156608
FozleRabbi1 <fozlerabbishuvo@gmail.com>
2024-12-18 15:59:44 +0600

    little change in update blog page

--- src/Components/Dashboard/blog/UpdateBlog.jsx
+++ src/Components/Dashboard/blog/UpdateBlog.jsx
@@ -18,8 +18,7 @@ const UpdateBlog = () => {
                 <div className=" col-span-12 xl:col-span-8  " >
                     <div className="border rounded-lg p-4 mb-5">
                         <div>
-                            <span className="flex justify-between items-center mb-5" >
-                                <h2 className="font-semibold text-[24px] " >Header Image</h2>
+                            <span className="flex justify-end items-center mb-5" >
                                 <button className="border border-[#1A9835] text-[30px] rounded " > <CiEdit className="text-[#1A9835]   " /> </button>
                             </span>
                             {/* ========================= need condition in image follow the figma ========================= */}
@@ -27,8 +26,7 @@ const UpdateBlog = () => {
 
                         </div>
                         <div className="mt-4" >
-                            <span className="flex justify-between items-center mb-5" >
-                                <h2 className="font-semibold text-[24px] " >Header Text</h2>
+                            <span className="flex justify-end items-center mb-5" >
                                 <button className="border border-[#1A9835] text-[30px] rounded " > <CiEdit className="text-[#1A9835]   " /> </button>
                             </span>
                             <p className="text-[#141D2A]  " >Essential Travel Hacks for Stress-Free Adventures</p>
@@ -37,8 +35,7 @@ const UpdateBlog = () => {
 
                     <div className="border rounded-lg p-4 mb-5">
                         <div>
-                            <span className="flex justify-between items-center mb-5" >
-                                <h2 className="font-semibold text-[24px] " >Body Image</h2>
+                            <span className="flex justify-end items-center mb-5" >
                                 <span className="flex items-center gap-5" >
                                     <button className="border border-[#1A9835] text-[30px] rounded " > <CiEdit className="text-[#1A9835]   " /> </button>
                                     <button className="border border-[#EB3D4D] text-[30px] rounded " > <RiDeleteBin5Line className="text-[#EB3D4D]   " /> </button>
@@ -49,8 +46,7 @@ const UpdateBlog = () => {
                         </div>
 
                         <div className="mt-4" >
-                            <span className="flex justify-between items-center mb-5" >
-                                <h2 className="font-semibold text-[24px] " >Body text</h2>
+                            <span className="flex justify-end items-center mb-5" >
                                 <button className="border border-[#1A9835] text-[30px] rounded " > <CiEdit className="text-[#1A9835]   " /> </button>
                             </span>
                             <p className="text-[#141D2A]  " >Traveling is one of life’s greatest joys, but the logistics can sometimes be overwhelming. From packing to navigating new places, small inconveniences can add up. That’s why we’ve compiled this guide of essential travel hacks to ensure your journey is smooth, stress-free, and full of unforgettable memories.</p>
@@ -66,8 +62,7 @@ const UpdateBlog = () => {
                         <h2 className="font-semibold text-[#141D2A] mb-2 " >Pack Smart, Pack Light</h2>
 
                         <div>
-                            <span className="flex justify-between items-center mb-5" >
-                                <h2 className="font-semibold text-[24px] " >Body Image</h2>
+                            <span className="flex justify-end items-center mb-5" >
                                 <span className="flex items-center gap-5" >
                                     <button className="border border-[#1A9835] text-[30px] rounded " > <CiEdit className="text-[#1A9835]   " /> </button>
                                     <button className="border border-[#EB3D4D] text-[30px] rounded " > <RiDeleteBin5Line className="text-[#EB3D4D]   " /> </button>
