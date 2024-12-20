acdd0d094248d7c50be1395e7cbcb9a47b107269
Tariq-Monowar <hassainseam@gmail.com>
2024-12-20 09:26:27 +0600

    updated

--- src/Pages/Auth/SignUp.jsx
+++ src/Pages/Auth/SignUp.jsx
@@ -2,8 +2,26 @@ import { useNavigate } from "react-router-dom";
 import ParentAuthComponent from "../../Shared/ParentComponent/ParentAuthComponent";
 import heroImage from "../../assets/Images/beach.jpg";
 import logo from "../../assets/logo.svg";
+import { useForm } from "react-hook-form";
+
 const SignUp = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
+
+  const SignUp = () => {
+    const navigate = useNavigate();
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+    } = useForm();
+  };
+
+  
+  const onSubmit = (data) => {
+    console.log("Form Data:", data);
+    // Handle form submission logic, e.g., API calls
+  };
+
   return (
     <ParentAuthComponent>
       <div className="grid grid-cols-1 md:grid-cols-5 h-full ">
@@ -13,9 +31,11 @@ const SignUp = () => {
         <div className="h-full w-full md:col-span-3">
           <div className="lg:m-20 m-5">
             <img src={logo} alt="" />
-            <h1 className="font-extrabold text-[32px] mt-10">Create New Account</h1>
+            <h1 className="font-extrabold text-[32px] mt-10">
+              Create New Account
+            </h1>
             <h5 className="text-[#72777F] text-[16px] mt-3">
-               Please enter your details.
+              Please enter your details.
             </h5>
             <div>
               <p className="text-[18px] font-medium mt-5">Name</p>
@@ -43,11 +63,23 @@ const SignUp = () => {
             </div>
             <div className="mt-1 flex justify-between">
               <div>
-                <p className="inline text-[14px]">Must be at least 8 characters</p>
+                <p className="inline text-[14px]">
+                  Must be at least 8 characters
+                </p>
               </div>
             </div>
-            <button className="primary_bg p-3 w-full rounded-lg text-white text-[16px] font-semibold mt-6">Sign Up</button>
-          <div className="text-[#475467] mt-8 text-center">Already have account ? <span onClick={()=>navigate('/login')} className="primary_text font-semibold cursor-pointer">Log in</span></div>
+            <button className="primary_bg p-3 w-full rounded-lg text-white text-[16px] font-semibold mt-6">
+              Sign Up
+            </button>
+            <div className="text-[#475467] mt-8 text-center">
+              Already have account ?{" "}
+              <span
+                onClick={() => navigate("/login")}
+                className="primary_text font-semibold cursor-pointer"
+              >
+                Log in
+              </span>
+            </div>
           </div>
         </div>
       </div>
