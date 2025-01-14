09163518f99bf3c7ffe5a8a7627a26c88e90a4e0
Tariq-Monowar <hassainseam@gmail.com>
2025-01-14 17:00:20 +0600

    new commit

--- src/utils/base_path.js
+++ src/utils/base_path.js
@@ -1,1 +1,1 @@
-export const base_url = "https://gourmet-walked-impacts-european.trycloudflare.com";
+export const base_url = "https://perhaps-safari-captured-states.trycloudflare.com" // "https://gourmet-walked-impacts-european.trycloudflare.com";
